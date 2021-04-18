07f8ab12d053726e8e668edbc3a3012e7ab71813
Dylan McDonald <dylan@dlmconsulting.org>
2021-04-18 03:03:37 -0400

    UI changes, basic functionality

--- Resolutioner/StandardViews.swift
+++ Resolutioner/StandardViews.swift
@@ -8,10 +8,37 @@
 import UIKit
 
 class SFMonoTextField: UITextField {
-
-   
     override func draw(_ rect: CGRect) {
         self.font = UIFont.monospacedSystemFont(ofSize: self.font!.pointSize, weight: .regular)
     }
+}
 
+class SFMonoLabel: UILabel {
+    override func draw(_ rect: CGRect) {
+        self.font = UIFont.monospacedSystemFont(ofSize: self.font!.pointSize, weight: .regular)
+    }
+}
+
+func applySFMonoFont(forLabel: UILabel, withWeight: UIFont.Weight) {
+    forLabel.font = UIFont.monospacedSystemFont(ofSize: forLabel.font!.pointSize, weight: withWeight)
+}
+
+class StandardButton: UIButton {
+    override func draw(_ rect: CGRect) {
+        self.layer.masksToBounds = true
+        
+        if self.frame.height != self.frame.width {
+            self.layer.cornerRadius = 10
+            self.layer.cornerCurve = .continuous
+        } else {
+            self.layer.cornerRadius = self.frame.height / 2
+        }
+        
+        self.layer.borderWidth = 1.5
+        self.layer.borderColor = UIColor.label.withAlphaComponent(0.075).cgColor
+    }
+    
+    override class func didChangeValue(forKey key: String) {
+        print("changed!!")
+    }
 }
--- Resolutioner/ViewController.swift
+++ Resolutioner/ViewController.swift
@@ -9,16 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var aspectIsLocked = true
+    var oneToTwoRatio: Double = 9/16
+    var twoToOneRatio: Double = 16/9
+    
     @IBOutlet weak var mainView: UIView!
     @IBOutlet weak var firstResField: SFMonoTextField!
     @IBOutlet weak var secondResField: SFMonoTextField!
+    @IBOutlet weak var aspectLockButton: StandardButton!
     
     @IBOutlet weak var internalDismissKeyboardButton: UIButton!
+    
+    @IBOutlet weak var mainHeaderView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.layer.cornerRadius = 20
         mainView.layer.cornerCurve = .continuous
+        
+        mainHeaderView.layer.cornerRadius = 20
+        mainHeaderView.layer.cornerCurve = .continuous
+        mainHeaderView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        
+      
+        mainHeaderView.layer.shadowColor = UIColor.black.cgColor
+        mainHeaderView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        mainHeaderView.layer.shadowRadius = 5
+        if traitCollection.userInterfaceStyle == .dark {
+            mainHeaderView.layer.shadowOpacity = 0
+        } else {
+            mainHeaderView.layer.shadowOpacity = 0.4
+        }
+
+        internalDismissKeyboardButton.isHidden = true
+        
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        if traitCollection.userInterfaceStyle == .dark {
+            mainHeaderView.layer.shadowOpacity = 0
+        } else {
+            mainHeaderView.layer.shadowOpacity = 0.4
+        }
     }
 
     @IBAction func dismissKeyboards(_ sender: Any) {
@@ -27,11 +61,49 @@ class ViewController: UIViewController {
     }
     
     @IBAction func beganEditing(_ sender: Any) {
+//        currentRatio = Double(firstResField.text!)! / Double(secondResField.text!)!
         internalDismissKeyboardButton.isEnabled = true
+        internalDismissKeyboardButton.isHidden = false
+        self.internalDismissKeyboardButton.alpha = 0
+        UIView.animate(withDuration: 0.3) {
+            self.internalDismissKeyboardButton.alpha = 1
+        }
     }
     
     @IBAction func stoppedEditing(_ sender: Any) {
         internalDismissKeyboardButton.isEnabled = false
+        UIView.animate(withDuration: 0.3) {
+            self.internalDismissKeyboardButton.alpha = 0
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.31) {
+            self.internalDismissKeyboardButton.isHidden = true
+        }
+    }
+    @IBAction func lockAspectRatio(_ sender: Any) {
+        aspectIsLocked = !aspectIsLocked
+        if aspectIsLocked {
+            aspectLockButton.setTitle("Unlock Ratio", for: .normal)
+        } else {
+            aspectLockButton.setTitle("Lock Ratio", for: .normal)
+        }
+    }
+    
+    @IBAction func typedInRes1(_ sender: Any) {
+        if aspectIsLocked {
+            secondResField.text = "\(Int((Double(firstResField.text ?? "0") ?? 1) * oneToTwoRatio))"
+        }
+        if firstResField.text == "" {
+            secondResField.text = ""
+        }
+    }
+    
+    @IBAction func typedInRes2(_ sender: Any) {
+        if aspectIsLocked {
+            firstResField.text = "\(Int((Double(secondResField.text ?? "0") ?? 1) * twoToOneRatio))"
+        }
+        if secondResField.text == "" {
+            firstResField.text = ""
+        }
     }
     
 }
